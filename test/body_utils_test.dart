042dfc44307865e57a03efe3c7a06e47b1e6c990
Djcharles26 <jlaraanaya@gmail.com>
2022-09-28 08:19:45 -0500

    Temporaly commented body utils tests

--- test/body_utils_test.dart
+++ test/body_utils_test.dart
@@ -1,9 +1,3 @@
-
-
 void main() {
-
-    // expect(jsonField<String> (json, ["organization", "\$oid"], defaultValue: "Is null"), "Is null");
-    // expect (() => jsonField<String> (json, ["jsonBody"],  nullable: false), throwsA(isA<BodyException>()));
-
   
 }
