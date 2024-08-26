4c8b00c5e4312537a5a3c410a59148a25f54e169
Djcharles26 <jlaraanaya@gmail.com>
2024-08-25 20:42:28 -0600

    Updated how color parses values to use opacity

--- lib/utils/color_parse.dart
+++ lib/utils/color_parse.dart
@@ -6,27 +6,19 @@ Color? colorFromString(String? colorString) {
   /// Parse a color in hex format to [Color?] class.
   ///
   if (colorString == null) return null;
-  return Color(_hexToInt(colorString.toUpperCase()));
-}
-
-int _hexToInt(String hex) {
-  int val = 0;
-  int len = hex.length;
-  for (int i = 0; i < len; i++) {
-    int hexDigit = hex.codeUnitAt(i);
-    if (hexDigit >= 48 && hexDigit <= 57) {
-      val += (hexDigit - 48) * (1 << (4 * (len - 1 - i)));
-    } else if (hexDigit >= 65 && hexDigit <= 70) {
-      // A..F
-      val += (hexDigit - 55) * (1 << (4 * (len - 1 - i)));
-    } else if (hexDigit >= 97 && hexDigit <= 102) {
-      // a..f
-      val += (hexDigit - 87) * (1 << (4 * (len - 1 - i)));
-    } else {
-      throw const FormatException("Invalid hexadecimal value");
-    }
+  if (!RegExp(r'^[0-9A-Fa-f]{8}$').hasMatch(colorString)) {
+    throw const FormatException(
+      "Color not formatted!. Format must be [0-9A-F-a-f]{8}"
+    );
   }
-  return val;
+  final intColor = int.parse(colorString, radix: 16);
+   // Extraer los valores de opacidad, R, G, y B
+  int opacity = (intColor >> 24) & 0xFF;
+  int red = (intColor >> 16) & 0xFF;
+  int green = (intColor >> 8) & 0xFF;
+  int blue = intColor & 0xFF;
+
+  return Color.fromARGB(opacity, red, green, blue);
 }
 
 Color getRandomColor () {
